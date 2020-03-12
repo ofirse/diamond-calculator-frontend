1b88bb1243534c8f33c6a80979e046661c6a20c0
ofir <ofirse@mta.ac.il>
2020-03-12 13:58:17 +0200

    promotions styling

--- src/components/Promotion.js
+++ src/components/Promotion.js
@@ -2,15 +2,17 @@ import React from 'react';
 import {Card, CardBody, CardTitle} from 'reactstrap';
 
 const Promotion = props => {
-    return <div className="row mb-5">
-        <i className={props.myIcon + " mr-3"} id="promotion-icon"/>
-            <Card className="promotion-card col">
-                <CardTitle className="mb-0 text-black">
-                    {props.promotiontitle}
-                </CardTitle>
-                <CardBody>{props.promotionBody}</CardBody>
-            </Card>
+    return <div className="promotion d-flex">
+        <div className="promotion-icon d-flex justify-content-center">
+            <i className={props.myIcon}/>
         </div>
+        <div className="promotion-card px-3">
+            <div className="promotion-title">
+                {props.promotiontitle}
+            </div>
+            <div className="promotion-body">{props.promotionBody}</div>
+        </div>
+    </div>
 };
 
 export default Promotion
--- src/components/PromotionsList.js
+++ src/components/PromotionsList.js
@@ -4,24 +4,34 @@ import Promotion from "./Promotion";
 const PromotionsList = props => {
 
     return <div className="promotions-list">
-        <div className="row">
-            <div className="col">
-            <hr className="promotion-line"/>
-             <div className="h3">Promotions</div>
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                    <hr className="promotion-line"/>
+                    <div className="h3">Promotions</div>
+                </div>
             </div>
-        </div>
-        <div className="promotions-container row mt-3 ">
-            <div className="col-sm">
-                <Promotion myIcon="fa fa-basketball-ball fa-3x " promotiontitle="NBA PROMOTION WITH 100% UP TO 50€ FOR YOU!" promotionBody="NBA sports betting awaits you, and so does your 100% up to 50€ deposit bonus!"/>
-                <Promotion myIcon="fa fa-coins fa-3x " promotiontitle="500 € CASINO WELCOME BONUS!" promotionBody="Start your 18bet casino trip with a whopping 500 € bonus! If you are making your first steps with 18bet casino now, we would like to give you a perfect start with a huge bonus your first deposit!"/>
+            <div className="promotions-container row">
+                <div className="col-sm">
+                    <Promotion myIcon="fa fa-basketball-ball fa-3x " promotiontitle="NBA PROMOTION WITH 100% UP TO 50€ FOR YOU!" promotionBody="NBA sports betting awaits you, and so does your 100% up to 50€ deposit bonus!"/>
+                </div>
+                <div className="col-sm">
+                    <Promotion  myIcon="fa fa-ghost fa-3x " promotiontitle="GRAB A SHARE OF 200€ IN THIS SPOOKTACULAR TOURNAMENT!" promotionBody="The Halloween spirit hovers around and you also feel it, right? It only leaves cold chills behind him. Well, not only that actually..."/>
+                </div>
+                <div className="col-sm">
+                    <Promotion  myIcon="fa fa-coins fa-3x " promotiontitle="BOOST YOUR BALANCE WITH OUR 50% UP TO 50€ RELOAD BONUS" promotionBody="We've got a 50% up to 50€ reload bonus to give you that competitive edge and ensure a good time at Babibet!"/>
+                </div>
             </div>
-            <div className="col-sm">
-               <Promotion  myIcon="fa fa-coins fa-3x " promotiontitle="GRAB A SHARE OF 200€ IN THIS SPOOKTACULAR TOURNAMENT!" promotionBody="The Halloween spirit hovers around and you also feel it, right? It only leaves cold chills behind him. Well, not only that actually..."/>
-               <Promotion myIcon="fa fa-coins fa-3x " promotiontitle="REQUEST YOUR PRIVATE SPORTS BONUS" promotionBody="You want a sports bonus? Do not hesitate to contact us and request your own personal one!"/>
-            </div>
-            <div className="col-sm">
-                <Promotion  myIcon="fa fa-coins fa-3x " promotiontitle="jjj" promotionBody="hhh"/>
-                <Promotion myIcon="fa fa-coins fa-3x " promotiontitle="gggg" promotionBody="ttt"/>
+            <div className="promotions-container row">
+                <div className="col-sm">
+                    <Promotion myIcon="fa fa-dice fa-3x " promotiontitle="500 € CASINO WELCOME BONUS!" promotionBody="Start your 18bet casino trip with a whopping 500 € bonus! If you are making your first steps with 18bet casino now, we would like to give you a perfect start with a huge bonus your first deposit!"/>
+                </div>
+                <div className="col-sm">
+                    <Promotion myIcon="fa fa-futbol fa-3x " promotiontitle="REQUEST YOUR PRIVATE SPORTS BONUS" promotionBody="You want a sports bonus? Do not hesitate to contact us and request your own personal one!"/>
+                </div>
+                <div className="col-sm">
+                    <Promotion myIcon="fa fa-door-open fa-3x " promotiontitle="100% up to 100€ Welcome Bonus" promotionBody="Get an explosive start in sports betting thanks to our amazing Welcome Bonus offer. Claim 100% extra cash up to 100€ and start making your winning picks right now!"/>
+                </div>
             </div>
         </div>
     </div>
